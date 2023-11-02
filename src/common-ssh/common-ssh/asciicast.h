a9f858211a4ab7d25ee3af72d3db26f8fbf76872
amityahav <amityahav12@gmail.com>
2023-11-02 17:39:10 +0200

    impl asciicast

--- src/common-ssh/common-ssh/asciicast.h
+++ src/common-ssh/common-ssh/asciicast.h
@@ -1,11 +1,13 @@
+#include <time.h>
 #include <stdlib.h>
+
 
 typedef struct asciicast_event {
 
     /*
     the timestamp of the event
     */
-    float timestamp;
+    time_t timestamp;
 
     /*
     the mode of the event
@@ -19,17 +21,35 @@ typedef struct asciicast_event {
 
 } asciicast_event;
 
+asciicast_event *asciicast_event_create(time_t timestamp, char mode, char *buffer, int bytes_read);
+void free_asciicast_event(asciicast_event *e);
+
 typedef struct slice {
-   asciicast_event *array;
+   asciicast_event* *array;
+
    size_t size;
+
    size_t cap; 
+
 } slice;
 
-slice* append(slice *s, asciicast_event e);
+slice* append(slice *s, asciicast_event *e);
 void free_slice(slice *s);
 
 typedef struct asciicast_recording {
    char input_start;
-   slice input_events;
-   slice output_events;
+
+   time_t epoch;
+
+   time_t timestamp;
+
+   time_t seconds;
+
+   slice *input_events;
+
+   slice *output_events;
+
 } asciicast_recording;
+
+asciicast_recording* asciicast_recording_create();
+void free_asciicast_recording(asciicast_recording *rec);
