9fd45d7a08d15c184047b3854522d7db8a29dde3
yellin <jonathan@cyolo.io>
2025-04-18 07:32:03 +0000

    [CY-0000] bump version to 1.5.5-2

--- src/guacd/daemon.c
+++ src/guacd/daemon.c
@@ -328,7 +328,7 @@ int main(int argc, char* argv[]) {
     /* If requested, simply print version and exit, without initializing the
      * logging system, etc. */
     if (config->print_version) {
-        printf("Guacamole proxy daemon (guacd) - Cyolo build - version 1.5.5-1");
+        printf("Guacamole proxy daemon (guacd) - Cyolo build - version 1.5.5-2");
         exit(EXIT_SUCCESS);
     }
 
@@ -337,7 +337,7 @@ int main(int argc, char* argv[]) {
     openlog(GUACD_LOG_NAME, LOG_PID, LOG_DAEMON);
 
     /* Log start */
-    guacd_log(GUAC_LOG_INFO, "Guacamole proxy daemon (guacd) - Cyolo build - version 1.5.5-1");
+    guacd_log(GUAC_LOG_INFO, "Guacamole proxy daemon (guacd) - Cyolo build - version 1.5.5-2");
 
     /* Get addresses for binding */
     if ((retval = getaddrinfo(config->bind_host, config->bind_port,
